577dc618792626d40fc3eecfdfe0f77f103ec58f
Benjamin Derksen <benjamin.derksen.98@gmail.com>
2025-12-17 00:23:44 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::{env, fs, os::unix::fs::PermissionsExt};
+use std::os::unix::process::CommandExt;
+use std::{
+    env,
+    fs::{self},
+    os::unix::fs::PermissionsExt,
+    path::PathBuf,
+    process::Command,
+};
 
 enum Builtin {
     Echo,
@@ -17,6 +24,29 @@ fn parse_builtin(name: &str) -> Option<Builtin> {
     }
 }
 
+fn find_executable_in_path(name: &str) -> Option<PathBuf> {
+    if let Some(path) = env::var_os("PATH") {
+        for dir in env::split_paths(&path) {
+            if let Ok(entries) = fs::read_dir(dir) {
+                for entry in entries.flatten() {
+                    let binary_name = entry.file_name();
+                    let meta = match fs::metadata(entry.path()) {
+                        Ok(m) => m,
+                        Err(_) => continue,
+                    };
+
+                    let perms = meta.permissions();
+                    if binary_name == name && (perms.mode() & 0o111) != 0 {
+                        return Some(entry.path());
+                    }
+                }
+            }
+        }
+    }
+
+    None
+}
+
 fn main() {
     loop {
         print!("$ ");
@@ -29,48 +59,47 @@ fn main() {
             .expect("failed to read line");
 
         let input = input.trim();
-        let (cmd_str, rest) = input.split_once(' ').unwrap_or((input, ""));
+        let (cmd_str, args) = input.split_once(' ').unwrap_or((input, ""));
 
         match parse_builtin(cmd_str) {
             Some(Builtin::Echo) => {
-                println!("{rest}")
+                println!("{args}")
             }
             Some(Builtin::Exit) => {
                 break;
             }
-            Some(Builtin::Type) => match parse_builtin(rest) {
-                Some(_) => println!("{rest} is a shell builtin"),
+            Some(Builtin::Type) => match parse_builtin(args) {
+                Some(_) => println!("{args} is a shell builtin"),
                 None => {
-                    let mut found = false;
+                    let exe = find_executable_in_path(args);
 
-                    if let Some(path) = env::var_os("PATH") {
-                        'outer: for dir in env::split_paths(&path) {
-                            if let Ok(entries) = fs::read_dir(dir) {
-                                for entry in entries.flatten() {
-                                    let binary_name = entry.file_name();
-                                    let meta = match fs::metadata(entry.path()) {
-                                        Ok(m) => m,
-                                        Err(_) => continue,
-                                    };
-
-                                    let perms = meta.permissions();
-                                    if binary_name == rest && (perms.mode() & 0o111) != 0 {
-                                        println!("{} is {}", rest, entry.path().display());
-                                        found = true;
-                                        break 'outer;
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    if !found {
-                        println!("{rest}: not found")
+                    match exe {
+                        Some(e) => println!("{} is {}", args, e.display()),
+                        None => println!("{}: not found", args),
                     }
                 }
             },
             _ => {
-                println!("{input}: command not found")
+                let exe = match find_executable_in_path(cmd_str) {
+                    Some(e) => e,
+                    None => {
+                        println!("{cmd_str}: not found");
+                        continue;
+                    }
+                };
+
+                let status = Command::new(exe)
+                    .arg0(cmd_str)
+                    .args(args.split_whitespace())
+                    .status();
+
+                match status {
+                    Ok(s) => s,
+                    Err(e) => {
+                        eprintln!("{cmd_str}: {e}");
+                        continue;
+                    }
+                };
             }
         }
     }
